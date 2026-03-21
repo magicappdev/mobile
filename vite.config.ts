fc1e62218f64b20be60a21b7be8772f21e5958b8
involvex <lukaswill97@gmail.com>
2026-03-21 23:35:40 +0100

    build: update Vite config for Vitest and legacy browser support
    Update import to use vitest/config for defineConfig to align with Vitest configuration. Set legacy plugin targets to exclude IE 11, enhancing browser compatibility. Comment out build target to rely on defaults or for future adjustments.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
 
 import legacy from '@vitejs/plugin-legacy'
+import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), legacy()],
+	plugins: [react(), legacy({targets: ['defaults', 'not IE 11']})],
 	build: {
-		target: 'es2020',
+		// target: 'es2020',
 		minify: false,
 	},
 	test: {
